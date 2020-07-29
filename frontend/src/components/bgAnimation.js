f24d4946630ab3f8a1a7d6fdf4b893a9195b40c7
maradelynie <32828701+maradelynie@users.noreply.github.com>
2020-07-28 21:46:47 -0300

    adding Animated BG

---
+++ frontend/src/components/bgAnimation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function BgAnimation() {
+  return (<>
+    <div className="bg__ball">&nbsp;</div>
+    <div className="bg__ball2">&nbsp;</div>
+    <div className="bg__ball3">&nbsp;</div>
+    <div className="bg__ball4">&nbsp;</div>
+    <div className="bg__ball5">&nbsp;</div>
+    </>
+  );
+}
+
+
--- frontend/src/components/mainContent.js
+++ frontend/src/components/mainContent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import TimeTracker from './timeTracker';
 import RecordsFrom from './recordsFrom';
+import BgAnimation from './bgAnimation';
 
 export default function MainContent() {
   return (
     <main className="main__container">
-      <div className="main__animation">animation</div>
+      <div className="main__animation"><BgAnimation/></div>
       <div className="main__card"><TimeTracker/></div>
       <div className="main__card"><RecordsFrom/></div>
     </main>
