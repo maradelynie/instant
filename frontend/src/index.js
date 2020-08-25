9c6be96618a1ec188b629b899ea5f4bd79c7a6a5
maradelynie <32828701+maradelynie@users.noreply.github.com>
2020-08-25 20:58:09 -0300

    slice scss, refactoring directories architecture corrections

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./global.scss"
+import "./styles/global.scss"
 
 import {Provider} from "react-redux";
 import Store from "./redux/storage";
